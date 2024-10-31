a943b70d03c0ee45ef8f36381d04287be74ec831
Marius-VladimirMatei <052583@edu.szf.at>
2024-10-31 15:12:56 +0100

    Update System_Administration_System_Final.c

--- System_Administration_System_Final.c
+++ System_Administration_System_Final.c
@@ -2,7 +2,8 @@
 #include <string.h>
 
 // Variable to define the array/struct size
-#define max_students 30
+#define max_students 2
+
 
 // Define the structure for articles using typedef
 typedef struct {
@@ -13,16 +14,14 @@ typedef struct {
 } Student;
 
 /*Functions prototypes |
-int* itemCount = > the pointer that indicates to the item number / order in the array */
+int* stundentCount = > the pointer that indicates to the item number / order in the array */
 
-
-
-//rename: addStudent searchStudent 
-void studentAdd(Student students[], int* studentCount);
+void addStudent(Student students[], int* studentCount);
 void showStudents(Student students[], int studentCount);
-void seekStudent(Student students[], int studentCount);
+void searchStudent(Student students[], int studentCount);
 void calculateAverageOfAllStudents(Student students[], int studentCount);
-float calculateAverageGrade(float grades[], int numGrades);
+float student_AverageGrade(float grades[], int numGrades);
+
 
 int main() {
     Student students[max_students];
@@ -32,6 +31,7 @@ int main() {
 
     while (1) {
         // Display the menu
+        printf_s("_________________________________________\n");
         printf_s("1. Add new student\n");
         printf_s("2. Show all students\n");
         printf_s("3. Search for a student\n");
@@ -46,13 +46,13 @@ int main() {
 
         switch (choice) {
         case 1:
-            studentAdd(students, &studentCount);
+            addStudent(students, &studentCount);
             break;
         case 2:
             showStudents(students, studentCount);
             break;
         case 3:
-            seekStudent(students, studentCount);
+            searchStudent(students, studentCount);
             break;
         case 4:
             calculateAverageOfAllStudents(students, studentCount);
@@ -68,48 +68,57 @@ int main() {
 
 
 // Function to add a student to the list
-void studentAdd(Student students[], int* studentCount) {
+void addStudent(Student students[], int* studentCount) {
     if (*studentCount >= max_students) {
         printf_s("Student list is full.\n");
-        printf_s("_________________________________________\n");
         return;
     }
 
+    // Variable newStudent (temporary holder) collects all data input form the user => store it in different arrays.
     Student newStudent;
+    // Variable that sets the number of grades for each student
+    int numGrades = 5;
+
 
     // Input student name
     printf_s("Enter the name of the student: ");
     fgets(newStudent.name, sizeof(newStudent.name), stdin);
 
     // Remove newline from fgets
-    newStudent.name[strcspn(newStudent.name, "\n")] = 0; 
+    newStudent.name[strcspn(newStudent.name, "\n")] = 0;
+
+
 
     // Input matriculation number
     printf_s("Enter matriculation number (positive integer): ");
+
+    // Input check for matriculation number
     while (scanf_s("%d", &newStudent.matriculationNumber) != 1 || newStudent.matriculationNumber <= 0) {
         printf_s("Invalid matriculation number. Enter a positive integer: ");
 
         // Clear input buffer
-        while (getchar() != '\n'); 
+        while (getchar() != '\n');
     }
+
 
     // Input grades
     printf_s("Enter 5 grades (between 1.0 and 5.0):\n");
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < numGrades; i++) {
         do {
-            printf_s("Grade %d: ", i + 1);
+            printf_s("Grade %d: ", i + 1);   // incrementation for every next grade until the 5th
             scanf_s("%f", &newStudent.grade[i]);
+
             if (newStudent.grade[i] < 1.0 || newStudent.grade[i] > 5.0) {
                 printf_s("Invalid grade. Please enter a grade between 1.0 and 5.0.\n");
-                printf_s("_________________________________________\n");
             }
         } while (newStudent.grade[i] < 1.0 || newStudent.grade[i] > 5.0);
     }
 
-    // Calculate and assign average grade
-    newStudent.average = calculateAverageGrade(newStudent.grade, 5);
 
-    // Add student to array
+    // Assign average grade to every student
+    newStudent.average = student_AverageGrade(newStudent.grade, numGrades);
+
+    // Add the new student to array
     students[*studentCount] = newStudent;
     (*studentCount)++;
 }
@@ -118,31 +127,29 @@ void studentAdd(Student students[], int* studentCount) {
 void showStudents(Student students[], int studentCount) {
     if (studentCount == 0) {
         printf_s("No students in the list.\n");
-        printf_s("_________________________________________\n");
         return;
     }
-    
+
 
     printf_s("Student list:\n");
     for (int i = 0; i < studentCount; i++) {
-        printf_s("Student %d:\n", i + 1);
+        printf_s("-------------- Student %d: ---------------\n", i + 1); //student number in list
         printf_s("Name: %s\n", students[i].name);
         printf_s("Matriculation Number: %d\n", students[i].matriculationNumber);
+
         printf_s("Grades: ");
         for (int j = 0; j < 5; j++) {
-            printf_s("%.2f ", students[i].grade[j]);
+            printf_s("%.2f || ", students[i].grade[j]);
         }
         printf_s("\nAverage: %.2f\n", students[i].average);
     }
-    printf_s("_________________________________________\n");
 }
 
 
 // Function to search for a student by matriculation number
-void seekStudent(Student students[], int studentCount) {
+void searchStudent(Student students[], int studentCount) {
     int matriculationNumber;
     printf_s("Enter the matriculation number to search for: \n");
-    printf_s("_________________________________________\n");
     scanf_s("%d", &matriculationNumber);
 
     for (int i = 0; i < studentCount; i++) {
@@ -151,15 +158,32 @@ void seekStudent(Student students[], int studentCount) {
             printf_s("Name: %s\n", students[i].name);
             printf_s("Matriculation Number: %d\n", students[i].matriculationNumber);
             printf_s("Grades: ");
+
             for (int j = 0; j < 5; j++) {
                 printf_s("%.2f ", students[i].grade[j]);
             }
-            printf_s("\nAverage: %.2f\n", students[i].average);
+            printf_s("\nAverage grade: %.2f\n", students[i].average);
             return;
         }
     }
     printf_s("Student with matriculation number %d not found.\n", matriculationNumber);
-    printf_s("_________________________________________\n");
+}
+
+
+
+// Function that calculates the average grade for a single student
+float student_AverageGrade(float grades[], int numGrades) {
+
+    //numGrades = the number of the grades in the array
+    if (numGrades == 0)
+        return 0.0; // Avoid division by zero
+
+    float sum = 0.0;
+    for (int i = 0; i < numGrades; i++) {
+        sum += grades[i];
+    }
+    return sum / numGrades;
+
 }
 
 
@@ -167,7 +191,6 @@ void seekStudent(Student students[], int studentCount) {
 void calculateAverageOfAllStudents(Student students[], int studentCount) {
     if (studentCount == 0) {
         printf_s("No students in the list.\n");
-        printf_s("_________________________________________\n");
         return;
     }
 
@@ -175,15 +198,6 @@ void calculateAverageOfAllStudents(Student students[], int studentCount) {
     for (int i = 0; i < studentCount; i++) {
         totalAverage += students[i].average;
     }
-    printf_s("Average grade of all students: %.2f\n", totalAverage / studentCount);
-    printf_s("_________________________________________\n");
-}
-
-// Helper function to calculate the average grade for a single student
-float calculateAverageGrade(float grades[], int numGrades) {
-    float sum = 0.0;
-    for (int i = 0; i < numGrades; i++) {
-        sum += grades[i];
-    }
-    return sum / numGrades;
+    //return totalAverage / studentCount;
+   printf_s("Average grade of all students: %.2f\n", totalAverage / studentCount);
 }
